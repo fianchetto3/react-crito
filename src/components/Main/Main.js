364ffe53331683549bd19918e621c9a63eab6938
simon <simonelenbrink@gmail.com>
2023-10-24 17:16:33 +0200

    structure

---
+++ src/components/Main/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ShowcaseSection from '../ShowcaseSection/ShowcaseSection'
+
+function Main() {
+  return (
+
+    <main>
+          <ShowcaseSection/>
+    </main>
+  
+    
+  )
+}
+
+export default Main
