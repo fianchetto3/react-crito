8bf3fd21c0329fa02aecd9cf3b430e358fe35b74
simon <simonelenbrink@gmail.com>
2023-10-31 12:05:00 +0100

    input changed

--- src/components/Contact/MessageBox.js
+++ src/components/Contact/MessageBox.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import FormInput from './FormInput'
 
 function MessageBox() {
   return (
+    
     <>
                     <section className="message-us">
                      <div className="container">
                             <div className="message-box">
                                 <div className="headline"> <h2>Leave us a message for any information.</h2> </div>
                                 <div className="search-bars">
-                                    <FormInput className ="name-bar" placeholder="Name*"  />
-                                    <FormInput className="email-bar" placeholder="Email*" />
-                                    <FormInput className="message-bar" placeholder="Message*" />
+                                     <form class="name-bar" action="">
+                                         <input type="text" placeholder="Name*"/>
+                                     </form>
+                                      <form class="email-bar" action="">
+                                          <input type="text" placeholder="Email*"/>
+                                      </form>
+                                        <form class="message-bar" action="">
+                                         <input type="text" placeholder="Your Message*"/>
+                                       </form>
                                 </div>
                                 <div className="send-btn">
                                     <a href=""> <button className="btn-yellow">Send Message <i className="fa-regular fa-arrow-up-right"></i></button> </a>
